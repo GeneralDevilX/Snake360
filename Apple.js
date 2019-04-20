55a37dea8b8f9c70b86b2b3feb360fb74640f80b
GeneralDevilX <evelynwijaya777@gmail.com>
2019-04-20 22:27:13 +0700

    modifikasi level, memperbaiki tampilan, menambah getter untuk kelas apple dan maze

--- Apple.js
+++ Apple.js
@@ -6,4 +6,10 @@ function Apple(besarApel){
 	this.getBesarApel = function(){
 		return this.besarApel;
 	}
+	this.getX = function(){
+		return this.x;
+	}
+	this.getY = function(){
+		return this.y;
+	}
 }
--- Snake.js
+++ Snake.js
@@ -2,12 +2,41 @@ function Snake(besarUlar){
 		this.x;
 		this.y;
 		this.panjangTubuhSaatIni = 15;
-		this.speed = 2;
+		this.speed;
 		this.tubuhSnake = [{x:this.x,y:this.y}];
 		this.besarUlar = besarUlar;
+		this.jarakAntaraTubuhUlar = 2;
 		
 		this.move = function(sudut){
-			this.x += Math.cos(sudut*Math.PI/180)*this.speed;
-			this.y += Math.sin(sudut*Math.PI/180)*this.speed;
-	}
+			this.x += Math.cos(sudut*Math.PI/180)*this.jarakAntaraTubuhUlar;
+			this.y += Math.sin(sudut*Math.PI/180)*this.jarakAntaraTubuhUlar;
+		}
+		
+		this.setSpeed = function(speed){
+			this.speed = speed;
+		}
+		
+		this.getSpeed = function(){
+			return this.speed;
+		}
+		
+		this.getBesarUlar = function(){
+			return this.besarUlar;
+		}
+		
+		this.getTubuhSnake = function(){
+			return this.tubuhSnake;
+		}
+		
+		this.getPanjangTubuh = function(){
+			return this.panjangTubuhSaatIni;
+		}
+		
+		this.getX = function(){
+			return this.x;
+		}
+		
+		this.getY = function(){
+			return this.y;
+		}
 }
